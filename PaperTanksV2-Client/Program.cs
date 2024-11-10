b262df827217b0f47be82d46cd9d6bd2fddac761
Dean Van Greunen <deanvg9000@gmail.com>
2024-11-10 12:29:43 +0200

    Working c# project

--- PaperTanksV2-Client/Program.cs
+++ PaperTanksV2-Client/Program.cs
@@ -61,3 +61,21 @@ namespace PaperTanksV2_Client
 
     }
 }
+
+
+/* TODO List:
+ * - Create Game Loop (init, input, update, render)
+ * - Create Resource Manager (Images, Fonts, Audio)
+ * - Create PageState Interface
+ * - Create Splash Page (single page)
+ * - Create Main Menu Page (double page) [New Game, Load Game, MultiPlayer (Client<->Server), Downloadable Content, Settings, About, Exit]
+ * - Create Downloadable Content Page (double page) [A List of downloadable content, multiplayer map packs, campaign level extensions]
+ * - Create Settings Page (double page) [Input Bindings, SFX Volume, Music Volume, Voice Over Volume, SpeedRun Timer Enabled]
+ * - Create About Page (double page) [Game Build Version)
+ * - Create Credits Page (double page) [Only Shown when the base campaign is completed]
+ * 
+ * GameClient: .... 
+ * 
+ * Future Planned Feature:
+ * - Level Editor
+ */
