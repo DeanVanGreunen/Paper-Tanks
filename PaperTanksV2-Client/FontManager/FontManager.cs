5f24aee6c74c5b93d8c7e1dde83c1563f55cff39
Dean Van Greunen <deanvg9000@gmail.com>
2024-11-24 20:35:06 +0200

    Made bitmaps of fonts to use, easier than coding the entire font manager, and renderer systems, this is fast and loose, does require some rework if new characters are added, but its in 4k so the text is quite high res, anyway, the client will end up using lower res, or up-to-max of 4k res. since the game is coded to render in 4k by default and then scale down

--- PaperTanksV2-Client/FontManager/FontManager.cs
+++ PaperTanksV2-Client/FontManager/FontManager.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace PaperTanksV2_Client.FontManager
 {
+    // character block size 103 x 169
+    // (Pencil) row 0: A-Z0123456789.,()
+    // (Pencil) row 1: a-z
+    // (Quick Pencil) row 2: A-Z0123456789.,()
+    // (Quick Pencil) row 3: a-z
+    // (QuickSand) row 4: A-Z0123456789.,()
+    // (QuickSand) row 5: a-z
     class FontManager
     {
 
